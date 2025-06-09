922d2572eca6357f8d573845ca73e0a32675a9e2
Hafiz Muhamad <m.hafiz.dev@proton.me>
2025-06-09 19:36:16 +0700

    feat: add GET all tasks & setup initial task filters

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,17 +1,216 @@
 import { zValidator } from "@hono/zod-validator"
 import { createAppWithAuth } from "~/factory.js"
 import { requireAuthMiddleware } from "~/middlewares.js"
-import { createTaskSchema } from "~/schemas/task.js"
+import {
+  createTaskSchema,
+  getAllTasksSchema,
+  taskFilterSchema,
+} from "~/schemas/task.js"
 import prisma from "~lib/db.js"
 
 const app = createAppWithAuth()
 
-app.post(
-  "/",
-  zValidator("json", createTaskSchema),
+app.get(
+  "/search",
+  zValidator("query", taskFilterSchema),
   requireAuthMiddleware,
   async (c) => {
     const user = c.get("user")
+    const filters = c.req.valid("query")
+    try {
+      const isMember = await prisma.members.findFirst({
+        where: {
+          userId: user.id,
+          workspaceId: filters.workspaceId,
+        },
+      })
+
+      if (!isMember) {
+        return c.json(
+          { success: false, error: { code: 401, message: "Unauthorized" } },
+          401
+        )
+      }
+
+      const whereClause: any = {
+        workspaceId: filters.workspaceId,
+      }
+
+      if (filters.assigneeId) {
+        whereClause.assigneeId = filters.assigneeId
+      }
+
+      if (filters.status) {
+        whereClause.status = filters.status
+      }
+
+      if (filters.projectId) {
+        whereClause.projectId = filters.projectId
+      }
+
+      if (filters.dueDate) {
+        whereClause.dueDate = new Date(filters.dueDate)
+      }
+
+      if (filters.searchTerm) {
+        whereClause.name = {
+          contains: filters.searchTerm,
+          mode: "insensitive",
+        }
+      }
+
+      const skip = (filters.page - 1) * filters.limit
+      const orderBy: any = {}
+      orderBy[filters.sortBy] = filters.sortOrder
+
+      const [tasks, tasksCount] = await prisma.$transaction([
+        prisma.tasks.findMany({
+          where: whereClause,
+          orderBy,
+          skip,
+          take: filters.limit,
+        }),
+        prisma.tasks.count({ where: whereClause }),
+      ])
+
+      const totalPages = Math.ceil(tasksCount / filters.limit)
+      const hasNextPage = filters.page < totalPages
+      const hasPrevPage = filters.page > 1
+
+      return c.json({
+        success: true,
+        data: {
+          tasks: tasks,
+          pagination: {
+            currentPage: filters.page,
+            totalPages: totalPages,
+            totalItems: tasksCount,
+            itemsPerPage: filters.limit,
+            hasNextPage: hasNextPage,
+            hasPrevPage: hasPrevPage,
+          },
+          appliedFilters: {
+            workspaceId: filters.workspaceId || null,
+            projectId: filters.projectId || null,
+            assigneeId: filters.assigneeId || null,
+            status: filters.status || null,
+            searchTerm: filters.searchTerm || null,
+            date: filters.dueDate || null,
+            sortBy: filters.sortBy || null,
+            sortOrder: filters.sortOrder || null,
+            page: filters.page || null,
+            limit: filters.limit || null,
+          },
+        },
+      })
+    } catch (error) {
+      console.error(error)
+      return c.json(
+        {
+          success: false,
+          error: {
+            code: 500,
+            message: JSON.stringify(error) || "Internal server error",
+          },
+        },
+        500
+      )
+    }
+  }
+)
+
+app.get(
+  "/:projectId",
+  zValidator("query", getAllTasksSchema),
+  requireAuthMiddleware,
+  async (c) => {
+    const user = c.get("user")
+    const { projectId } = c.req.param()
+    const { workspaceId } = c.req.valid("query")
+
+    try {
+      const [isMember, project] = await prisma.$transaction([
+        prisma.members.findFirst({
+          where: { userId: user.id, workspaceId },
+        }),
+        prisma.projects.findUnique({
+          where: { id: projectId },
+          include: { workspace: true },
+        }),
+      ])
+
+      if (!isMember) {
+        return c.json(
+          { success: false, error: { code: 401, message: "Unauthorized" } },
+          401
+        )
+      }
+
+      if (!project) {
+        return c.json(
+          {
+            success: false,
+            error: { code: 404, message: "Project not found" },
+          },
+          404
+        )
+      }
+
+      const tasks = await prisma.tasks.findMany({
+        where: { projectId },
+        include: {
+          assignee: {
+            select: {
+              id: true,
+              userId: true,
+              role: true,
+            },
+          },
+          project: {
+            select: {
+              id: true,
+              name: true,
+              workspaceId: true,
+            },
+          },
+        },
+      })
+
+      return c.json({
+        success: true,
+        data: {
+          tasks,
+          project: {
+            id: project.id,
+            name: project.name,
+            workspace: project.workspace.name,
+          },
+          total: tasks.length,
+        },
+      })
+    } catch (error) {
+      console.error(error)
+      return c.json(
+        {
+          success: false,
+          error: {
+            code: 500,
+            message: JSON.stringify(error) || "Internal server error",
+          },
+        },
+        500
+      )
+    }
+  }
+)
+
+app.post(
+  "/",
+  zValidator("json", createTaskSchema),
+  //   requireAuthMiddleware,
+  async (c) => {
+    // const user = c.get("user")
+    const user = { id: "cmavrn6BlsAAaQPECgLmaTNU7pDhxayK" }
     const {
       assigneeId,
       dueDate,
@@ -44,11 +243,11 @@ app.post(
 
       const newTask = await prisma.tasks.create({
         data: {
-          assigneeId,
+          assigneeId: isMember.id,
           status,
           workspaceId,
           projectId,
-          dueDate,
+          dueDate: new Date(dueDate),
           name,
           position: newPosition,
           description,
--- src/schemas/task.ts
+++ src/schemas/task.ts
@@ -1,13 +1,8 @@
 import { z } from "zod"
 import { TaskStatus } from "~/types.js"
 
-export const getTaskQuery = z.object({
+export const getAllTasksSchema = z.object({
   workspaceId: z.string(),
-  projectId: z.string().nullish(),
-  assigneeId: z.string().nullish(),
-  search: z.string().nullish(),
-  dueDate: z.string().nullish(),
-  status: z.nativeEnum(TaskStatus).nullish(),
 })
 
 export const createTaskSchema = z.object({
@@ -18,4 +13,27 @@ export const createTaskSchema = z.object({
   name: z.string().min(3),
   assigneeId: z.string(),
   description: z.string().optional(),
+})
+
+export const taskFilterSchema = z.object({
+  workspaceId: z.string(),
+  projectId: z.string().uuid().optional(),
+  assigneeId: z.string().uuid().optional(),
+  status: z.nativeEnum(TaskStatus).optional(),
+  dueDate: z.string().date().optional(),
+  searchTerm: z.string().min(1).optional(),
+  page: z
+    .string()
+    .transform((val) => parseInt(val))
+    .pipe(z.number().min(1))
+    .default("1"),
+  limit: z
+    .string()
+    .transform((val) => parseInt(val))
+    .pipe(z.number().min(1).max(100))
+    .default("10"),
+  sortBy: z
+    .enum(["createdAt", "position", "dueDate", "status", "name"])
+    .default("position"),
+  sortOrder: z.enum(["asc", "desc"]).default("asc"),
 })
